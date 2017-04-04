315fc7494716025b176c226855d5a8e98bdf2103
Tanguy Serrat <tanguyserrat@eduroam-int-dhcp-97-62-199.ucl.ac.uk>
2017-04-04 23:38:27 +0100

    fixed weights

--- code/p2/models/utils.py
+++ code/p2/models/utils.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 def weights(shape, name):
     return tf.get_variable('W_%s' % name, shape, 
 				initializer=tf.truncated_normal_initializer(
-								mean=0.0, stddev=0.1, dtype=tf.float32))
+								mean=0.0, stddev=0.01, dtype=tf.float32))
 
 def biases(shape, name):
     return tf.get_variable('b_%s' % name, shape, 
