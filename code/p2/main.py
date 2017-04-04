2041195a4b2a2c94d06014af16146f328b0ece5d
Tanguy Serrat <tanguyserrat@eduroam-int-dhcp-97-62-199.ucl.ac.uk>
2017-04-04 19:32:05 +0100

    setting training params

--- code/p2/main.py
+++ code/p2/main.py
@@ -13,16 +13,40 @@ from core.qlearning import do_online_qlearning
 
 #from core.running import run_multiple_trials
 
+ENVS = {
+    'pong': {
+        'gym_name': 'Pong-v3',
+        'learning_rate': 0.0001,
+    },
+    'pacman': {
+        'gym_name': 'MsPacman-v3',
+        'learning_rate': 0.0001, 
+    },
+    'boxing': {
+        'gym_name': 'Boxing-v3',
+        'learning_rate': 0.0001,
+    }
+}
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', nargs='?', type=str,
-                      help='Select model to train')
+    parser.add_argument('-e', '--env', nargs='?', type=str,
+                      help='Select environment to train')
+    parser.add_argument('--train', nargs='?', const=True, type=bool,
+                      default=False,
+                      help='If true, train model with fixed learning rate.')
 
     FLAGS, _ = parser.parse_known_args()
 
+    try:
+        tenv = ENVS[FLAGS.env]
+    except KeyError:
+        print('Env does not exist.')
+        raise
+
     dumps_dir = os.path.join(
-        os.path.dirname(__file__), 'dumps')      
+        os.path.dirname(__file__), 'dumps', FLAGS.env)      
     if not os.path.exists(dumps_dir):
         os.mkdir(dumps_dir)
 
@@ -30,32 +54,16 @@ if __name__ == '__main__':
 
     #learning_rates = [0.01, 0.1]
 
-    env = gym.make('Pong-v3')
-    test_env = gym.make('Pong-v3')
-    #history_buffer = HistoryBuffer(env, 2000, 300, 100)
+    env = gym.make(tenv['gym_name'])
+    test_env = gym.make(tenv['gym_name'])
 
-    #model1 = LinearValueFunctionApprox(4, 2)
-    #run_multiple_trials(env, history_buffer, model1, learning_rates, 10, dumps_dir)
-
-    #model2 = HiddenValueFunctionApprox(4, 2, 100)
-    #run_multiple_trials(env, history_buffer, model2, learning_rates, 10, dumps_dir)
-
-    #do_batch_qlearning(env, history_buffer, model, 0.0001)
-
-
-    epsilon_s = { 'start': 0.1, 'end': 0.005, 'decay': 2000 }
-    # model = HiddenValueFunctionApprox(4, 2, 100)
-
-
-    #do_online_qlearning(env, model, 0.001, epsilon_s, 2001, 300)
-
-    # for name, main_model in MODELS_LIST.items():
-
-    do_online_qlearning(env, test_env,
-                        model=AtariModel(env.action_space.n), 
-                        learning_rate=0.001,
-                        epsilon_s=epsilon_s, 
-                        target_model=AtariModel(env.action_space.n, varscope='target'),
-                        replay_buffer=ExperienceReplayBuffer(500000, 32))
+    if FLAGS.train:
+        epsilon_s = { 'start': 0.5, 'end': 0.005, 'decay': 2000 }
+        loss, means = do_online_qlearning(env, test_env,
+                            model=AtariModel(env.action_space.n), 
+                            learning_rate=0.0001,
+                            epsilon_s=epsilon_s, 
+                            target_model=AtariModel(env.action_space.n, varscope='target'),
+                            replay_buffer=ExperienceReplayBuffer(500000, 64))
 
 
