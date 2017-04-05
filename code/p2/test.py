1efaa00543b1a724bbb647703750fac19e1b8c8a
Tanguy Serrat <tanguy.serrat@gmail.com>
2017-04-05 07:27:08 -0400

    First try training

---
+++ code/p2/test.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+print(sess.run(c))
